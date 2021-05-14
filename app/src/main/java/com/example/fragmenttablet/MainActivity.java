2e5bf0882026e4dc9112491f4ea9cf85e519ea97
Le Quy Nhat <lequynhat1999@gmail.com>
2021-05-14 23:13:40 +0700

    add Toast "hello github"

--- app/src/main/java/com/example/fragmenttablet/MainActivity.java
+++ app/src/main/java/com/example/fragmenttablet/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements PassData{
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements PassData{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hello github", Toast.LENGTH_SHORT).show();
     }
 
     @Override
